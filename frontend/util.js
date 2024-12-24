0fec134c6eaadf9aa3ebb7cda93a402945dd8a1e
aditprq <143347564+aditprq@users.noreply.github.com>
2024-12-24 16:17:12 +0530

    errorfix

--- frontend/util.js
+++ frontend/util.js
@@ -1,4 +1,13 @@
 import {load} from '@cashfreepayments/cashfree-js';
-export const cashfree = await load({
-	mode: "production" //or production
-});
+let getcashfree; // Declare the variable globally
+
+async function initialize() {
+  getcashfree = await load({
+    mode: "production" // or production
+  });
+}
+
+initialize();  // Call the async function
+
+// Export a function that returns the cashfree value
+export const cashfree = () => getcashfree;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173} ,
-  build: {
-    target: 'esnext', // or 'es2022', 'chrome90', 'firefox90', etc.
-  }, 
-
+  server:{port:5173}
+  
 })
