90e4d4d3640680fe6db49573aa4027941bf0abaa
aditprq <143347564+aditprq@users.noreply.github.com>
2024-12-28 12:46:28 +0530

    css fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -76,7 +76,7 @@ const Navbar = () => {
 
           {/* Dropdown Menu */}
           {token && (
-            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="z-20 group-hover:block hidden absolute dropdown-menu right-0 pt-4">
               <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                 <p className="cursor-pointer hover hover:text-black">
                   My Profile
